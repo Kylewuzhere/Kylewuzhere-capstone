19c67e2ff38df8b17812a56e808e42b170f68394
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-15 11:30:51 +1200

    added colours to tailwind config

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -7,11 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'black': '#000000',
+        'orange': '#fe4600',
+        'grey-dark': '#1b1b1b',
+        'grey-button': '#8e8e93',
+        'grey-light': '#a5a5a5',
+        'blue-light': '#f0f7fb',
       },
+  
     },
   },
   plugins: [],
