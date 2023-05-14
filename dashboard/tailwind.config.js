f5f34b58f7f07977e2fac4188f3d38645fa0f79f
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-15 11:46:52 +1200

    Added border radius and font family to tailwind config

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,25 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
   theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'white': '#ffffff',
-        'black': '#000000',
-        'orange': '#fe4600',
-        'grey-dark': '#1b1b1b',
-        'grey-button': '#8e8e93',
-        'grey-light': '#a5a5a5',
-        'blue-light': '#f0f7fb',
-      },
-  
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
-  },
-  plugins: [],
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'orange': '#fe4600',
+      'grey-dark': '#1b1b1b',
+      'grey-button': '#8e8e93',
+      'grey-light': '#a5a5a5',
+      'blue-light': '#f0f7fb',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      // spacing: {
+      //   '128': '32rem',
+      //   '144': '36rem',
+      // },
+      borderRadius: {
+        'md': '10px',
+        'lg': '25px',
+      }
+    }
+  }
 }
