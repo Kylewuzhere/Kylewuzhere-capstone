3d4f1056b060d658a2c5741bcb7556e8dae458ee
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-16 11:43:39 +1200

    Problem resolved, I was wrapping the page content in a layout component and next.js does this automatically

--- dashboard/src/app/page.js
+++ dashboard/src/app/page.js
@@ -1,13 +1,18 @@
-import Layout from "./layout.js";
-
 export default function Home() {
   return (
-    <Layout>
-      <main className="flex min-h-screen flex-col items-center justify-between bg-grey-dark p-24">
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-          <button className="text-white bg-orange py-2 mr-4 px-4 rounded">LOGIN</button>
-        </div>
-      </main>
-    </Layout>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-grey-dark p-24">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <button className="text-white bg-orange py-2 mr-4 px-4 rounded">
+          LOGIN
+        </button>
+      </div>
+    </main>
   );
 }
