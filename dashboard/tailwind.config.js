7ee017e93f4130fcc47664d504d7e7024bad6f25
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-15 17:29:18 +1200

    updated tailwind.config arrangement to get google fonts to work correctly

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,37 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'black': '#000000',
-      'orange': '#fe4600',
-      'grey-dark': '#1b1b1b',
-      'grey-button': '#8e8e93',
-      'grey-light': '#a5a5a5',
-      'blue-light': '#f0f7fb',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
-      // spacing: {
-      //   '128': '32rem',
-      //   '144': '36rem',
-      // },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'black': '#000000',
+        'orange': '#fe4600',
+        'grey-dark': '#1b1b1b',
+        'grey-button': '#8e8e93',
+        'grey-light': '#a5a5a5',
+        'blue-light': '#f0f7fb',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+
       borderRadius: {
         'md': '10px',
         'lg': '25px',
       }
-    }
-  }
+    },
+  },
+  plugins: [],
 }
+
