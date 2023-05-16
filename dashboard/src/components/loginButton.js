4dd624a9aadd7b815a0941ff034685872b2ab222
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-16 15:17:36 +1200

    Create login and logout button components that use next-auth

---
+++ dashboard/src/components/loginButton.js
@@ -0,0 +1,19 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
+export const LoginButton = () => {
+  return (
+    <button
+      className="text-white bg-orange py-2 px-7 rounded text-sm"
+      onClick={() =>
+        signIn("okta", {
+          redirect: true,
+          callbackUrl: "http://localhost:3000/dashboard",
+        })
+      }
+    >
+      Sign in
+    </button>
+  );
+};
