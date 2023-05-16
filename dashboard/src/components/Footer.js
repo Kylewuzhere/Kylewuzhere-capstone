a2596f8f8656381c8642d15fe45548be7c76814a
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-16 12:40:04 +1200

    adjusted footer code to use tailwind

--- dashboard/src/components/Footer.js
+++ dashboard/src/components/Footer.js
@@ -2,13 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer
-      style={{ backgroundColor: "black", color: "white", padding: "10px" }}
-    >
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <span style={{ marginRight: "10px" }}>
-          ©2023 by Developers Institute
-        </span>
+    <footer className="bg-black text-white p-4">
+      <div className="flex items-center">
+        <span className="mr-4">©2023 by Developers Institute</span>
       </div>
     </footer>
   );
