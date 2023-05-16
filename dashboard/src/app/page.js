cc4af5bffae98feb0b917cafab44f7677694f43e
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-16 12:29:39 +1200

    adjusted some styling on the buttons to match the wireframes

--- dashboard/src/app/page.js
+++ dashboard/src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow flex flex-col items-center justify-center bg-grey-dark p-24">
         <div className="flex justify-center items-center">
-          <button className="text-white bg-orange py-2 mr-4 px-4 rounded">
+          <button className="text-white bg-orange py-2 pr-7 pl-7 rounded text-sm">
             LOGIN
           </button>
         </div>
--- dashboard/src/components/Header.js
+++ dashboard/src/components/Header.js
@@ -1,11 +1,14 @@
-import React from 'react';
-
+import React from "react";
 
 const Header = () => {
   return (
     <header className="bg-black text-white py-7 px-7 flex items-center justify-between">
-      <img src="/images/logo-transparent.png" alt="Logo" className="h-12 w-auto mx-4 my-5" />
-      <button className="text-white bg-orange py-2  mr-4 px-4 rounded">
+      <img
+        src="/images/logo-transparent.png"
+        alt="Logo"
+        className="h-12 w-auto mx-4 my-5"
+      />
+      <button className="text-white bg-orange py-3 pr-7 pl-7 rounded text-sm">
         LOG OUT
       </button>
     </header>
@@ -13,4 +16,3 @@ const Header = () => {
 };
 
 export default Header;
-
