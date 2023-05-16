82aa35eaec16892a1b93bbaadc9cd5690797216d
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-16 12:20:46 +1200

    re-enabled react strict mode as this was not the source of my problem for the double render of the header

--- dashboard/src/app/page.js
+++ dashboard/src/app/page.js
@@ -1,18 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-grey-dark p-24">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <button className="text-white bg-orange py-2 mr-4 px-4 rounded">
-          LOGIN
-        </button>
-      </div>
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow flex flex-col items-center justify-center bg-grey-dark p-24">
+        <div className="flex justify-center items-center">
+          <button className="text-white bg-orange py-2 mr-4 px-4 rounded">
+            LOGIN
+          </button>
+        </div>
+      </main>
+    </div>
   );
 }
