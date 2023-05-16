077ae1e81c73e9401c53c6ad8cf5251bcbd1bdf4
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-16 15:14:27 +1200

    move header from `app/layout.js` (moved to `dashboard/layout.js`) and rename imports

--- dashboard/src/app/layout.js
+++ dashboard/src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Footer from "@/components/Footer";
+import Provider from "@/components/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <Provider>
+          {children}
+          <Footer />
+        </Provider>
       </body>
     </html>
   );
