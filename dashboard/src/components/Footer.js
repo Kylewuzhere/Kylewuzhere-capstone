956bc6625bf4bdb1ba0e9eb7e7b524b610ded0de
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-16 12:07:46 +1200

    Added a footer component and imported it into the layout file

---
+++ dashboard/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ backgroundColor: "black", color: "white", padding: "10px" }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <span style={{ marginRight: "10px" }}>
+          ©2023 by Developers Institute
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
