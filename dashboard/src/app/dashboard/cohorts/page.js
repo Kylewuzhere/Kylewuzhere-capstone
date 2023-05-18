fb3d4359873070bcef52b0891929f89d085f853d
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-18 17:04:47 +1200

    add session check to cohorts page and refactor cohorts list

--- dashboard/src/app/dashboard/cohorts/page.js
+++ dashboard/src/app/dashboard/cohorts/page.js
@@ -1,28 +1,41 @@
-"use client";
-export default function Cohorts() {
-  const content = [
-    "Cohort 1",
-    "Cohort 2",
-    "Cohort 3",
-    "Cohort 4",
-    "Cohort 5",
-    "Cohort 6",
-    "Cohort 7",
-    "Cohort 8",
+import Link from "next/link";
+
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export default async function Cohorts() {
+  const cohorts = [
+    { name: "Cohort 1", id: "1" },
+    { name: "Cohort 2", id: "2" },
+    { name: "Cohort 3", id: "3" },
+    { name: "Cohort 4", id: "4" },
+    { name: "Cohort 5", id: "5" },
+    { name: "Cohort 6", id: "6" },
+    { name: "Cohort 7", id: "7" },
+    { name: "Cohort 8", id: "8" },
+    { name: "Cohort 9", id: "9" },
+    { name: "Cohort 10", id: "10" },
   ];
   // dummy cohorts
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
-    <>
-      <ul className="grid grid-cols-2 gap-10 m-10">
-        {content.map((item, index) => (
-          <li
-            key={index}
-            className="px-24 py-12 bg-blue-light border-2 border-black mx-16 rounded-md"
-          >
-            <p className="grid justify-center">{item}</p>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="flex flex-wrap gap-3 w-1/2">
+      {cohorts.map((cohort) => (
+        <Link
+          key={cohort.id}
+          className="flex justify-center items-center w-[45%] h-24 m-auto bg-white border-2 border-black rounded"
+          href={`/dashboard/cohorts/${cohort.id}`}
+        >
+          <span>{cohort.name}</span>
+        </Link>
+      ))}
+    </div>
   );
 }
