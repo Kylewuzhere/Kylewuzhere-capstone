91446162e56f823c662292c9a3ecf3399edb09c1
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-18 19:22:50 +1200

    Simplify layout page for dashboard

--- dashboard/src/app/dashboard/layout.js
+++ dashboard/src/app/dashboard/layout.js
@@ -3,13 +3,13 @@ import Sidebar from "@/components/sidebar";
 
 export default function RootLayout({ children }) {
   return (
-    <main className="bg-grey-dark h-screen">
+    <>
       <Header />
       {/* h-screen allows the background color: grey-dark to fully fill the background*/}
-      <div className="w-screen grid grid-cols-12">
-        <Sidebar />
-        <div className="col-span-10 bg-grey-md flex justify-center items-center h-screen pb-48">{children}</div>
+      <Sidebar />
+      <div className="bg-grey-md flex justify-center items-center h-screen ml-40">
+        {children}
       </div>
-    </main>
+    </>
   );
 }
