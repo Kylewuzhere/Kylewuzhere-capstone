dca28d66d530a65a07079361231ace8cce5bfcd8
KobeVasquezDI <86027430+KobeVasquezDI@users.noreply.github.com>
2023-05-18 12:47:24 +1200

    added filling grey-dark bg

--- dashboard/src/app/dashboard/layout.js
+++ dashboard/src/app/dashboard/layout.js
@@ -1,10 +1,14 @@
 import Header from "@/components/Header";
-
+import Sidebar from "@/components/sidebar";
 export default function RootLayout({ children }) {
   return (
     <>
       <Header />
-      {children}
+      {/* min-h-screen allows the background color: grey-dark to fully fill the background*/}
+      <div className="flex flex-grow flex-col md:flex-row flex-1 bg-grey-dark min-h-screen">
+        <Sidebar />
+        <main className="flex-1 bg-grey-dark">{children}</main>
+      </div>
     </>
   );
-};
+}
