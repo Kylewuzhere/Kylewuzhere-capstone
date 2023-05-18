a80fcc1f49900b5c183c462df72f81f8d238935b
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-18 17:01:32 +1200

    remove redundant code

--- dashboard/src/app/layout.js
+++ dashboard/src/app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
 import Provider from "@/components/provider";
-import InitialHeader from "@/components/InitialHeader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +15,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Provider>
-          {/* <InitialHeader /> */}
           {children}
           <Footer />
         </Provider>
