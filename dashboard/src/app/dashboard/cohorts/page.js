50781aba15b271a45fb689a173e086b172c36aad
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-22 10:27:30 +1200

    retrieve the list of cohorts from the database

--- dashboard/src/app/dashboard/cohorts/page.js
+++ dashboard/src/app/dashboard/cohorts/page.js
@@ -3,30 +3,10 @@ import Link from "next/link";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { GetCohorts } from "@/app/utils";
 
 export default async function Cohorts() {
-  const cohorts = [
-    { name: "Cohort 1", id: "1" },
-    { name: "Cohort 2", id: "2" },
-    { name: "Cohort 3", id: "3" },
-    { name: "Cohort 4", id: "4" },
-    { name: "Cohort 5", id: "5" },
-    { name: "Cohort 6", id: "6" },
-    { name: "Cohort 7", id: "7" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-    { name: "Cohort 8", id: "8" },
-  ];
-  // dummy cohorts
-
+  const cohorts = await GetCohorts();
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -38,10 +18,10 @@ export default async function Cohorts() {
       {cohorts.map((cohort) => (
         <Link
           key={cohort.id}
-          className="flex justify-center items-center p-8 bg-white border-2 border-black rounded"
-          href={`/dashboard/cohorts`}
+          className="cohortsListItem"
+          href={`/dashboard/cohorts/${cohort.id}`}
         >
-          <span className="text-sm">{cohort.name}</span>
+          {cohort.name}
         </Link>
       ))}
     </div>
