7b791ea11bc0b4c09aea2ffed90bc484cb715995
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-22 20:48:18 +1200

    implement GET route in /cohort

---
+++ dashboard/src/app/api/cohort/route.js
@@ -0,0 +1,9 @@
+import pool from "@/app/db";
+import { NextResponse } from "next/server";
+
+// /GET Cohorts route
+// Returns a list of all cohorts in the database
+export async function GET() {
+  const { rows } = await pool.query("SELECT id, name FROM cohort");
+  return NextResponse.json({ rows });
+}
