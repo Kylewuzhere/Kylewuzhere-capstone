44c1d7fc2fff3829cbedf92b0734281ef61b240a
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-22 15:18:04 +1200

    fix table name typo

--- dashboard/src/app/utils.js
+++ dashboard/src/app/utils.js
@@ -1,6 +1,6 @@
 import pool from "@/app/db";
 
 export async function GetCohorts() {
-  const { rows } = await pool.query("SELECT * FROM cohorts");
+  const { rows } = await pool.query("SELECT * FROM cohort");
   return rows;
 }
