d9dc31a10932cb2fa9b03520ee442d774fe2d029
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-22 17:11:26 +1200

    potentially fix hot-reloading

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+nextConfig = {
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 50,
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;
