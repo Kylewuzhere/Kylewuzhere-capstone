aa48d5b8d0f609f91ed2f063efbb76eb2ccd8fb1
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-23 18:32:11 +1200

    Simplify header component to conditionally render the signout button

--- dashboard/src/components/Header.js
+++ dashboard/src/components/Header.js
@@ -5,27 +5,12 @@ import { authOptions } from "../app/api/auth/[...nextauth]/route";
 
 const Header = async () => {
   const session = await getServerSession(authOptions);
-  if (session)
-    return (
-      <header className="h-12 bg-black text-white py-7 px-7 flex items-center justify-between">
-        <img
-          className="max-h-8"
-          src="/images/logo-transparent.png"
-          alt="Logo"
-        />
-        <LogoutButton />
-      </header>
-    );
-  else
-    return (
-      <header className="h-12 bg-black text-white py-7 px-7 flex items-center justify-between">
-        <img
-          className="max-h-8"
-          src="/images/logo-transparent.png"
-          alt="Logo"
-        />
-      </header>
-    );
+  return (
+    <header className="fixed top-0 w-full h-12 bg-black text-white py-7 px-7 flex items-center justify-between">
+      <img className="max-h-8" src="/images/logo-transparent.png" alt="Logo" />
+      {session && <LogoutButton />}
+    </header>
+  );
 };
 
 export default Header;
