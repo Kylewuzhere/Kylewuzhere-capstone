278b2625297ebcb33d7004dc3079c23e3f4c1117
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-23 22:13:14 +1200

    Created title component for cohorts page

---
+++ dashboard/src/components/CohortTitle.js
@@ -0,0 +1,17 @@
+const CohortTitle = async () => {
+  return (
+    <div className="w-full h-12  bg-grey-lighter text-black text-3xl font-semibold py-9 px-12 flex items-center justify-between">
+      <h1 className="ml-4">COHORTS</h1>
+      <div className="text-sm flex space-x-4 mr-4">
+        <button className="rounded border border-black bg-blue-light text-black px-10 py-2">
+          Sort
+        </button>
+        <button className="rounded border border-black bg-blue-light text-black px-10 py-2">
+          Filter
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CohortTitle;
