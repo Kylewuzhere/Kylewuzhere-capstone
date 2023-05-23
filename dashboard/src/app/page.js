f493591dca6496e07fe166082516eca7118d0679
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-23 18:31:35 +1200

    Remove old initital header from login page

--- dashboard/src/app/page.js
+++ dashboard/src/app/page.js
@@ -1,10 +1,8 @@
 import { LoginButton } from "../components/loginButton";
-import Header from "@/components/InitialHeader";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <div className="flex min-h-screen pb-24 items-center justify-center bg-grey-dark">
         <LoginButton />
       </div>
