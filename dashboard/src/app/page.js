ecea5d7312520f556bdf1de61aa4bc029d061bc5
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-23 18:49:23 +1200

    refactor login page

--- dashboard/src/app/page.js
+++ dashboard/src/app/page.js
@@ -2,10 +2,8 @@ import { LoginButton } from "../components/loginButton";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex min-h-screen pb-24 items-center justify-center bg-grey-dark">
-        <LoginButton />
-      </div>
-    </>
+    <div className="flex min-h-screen pb-24 items-center justify-center bg-grey-dark">
+      <LoginButton />
+    </div>
   );
 }
