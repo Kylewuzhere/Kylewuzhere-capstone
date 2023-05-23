a415fb96a7c36fb35565e91796681b4eba96d89a
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-05-23 18:47:45 +1200

    fix background color on cohorts page

--- dashboard/src/app/dashboard/cohorts/page.js
+++ dashboard/src/app/dashboard/cohorts/page.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 export default async function Cohorts() {
-
   async function GetCohorts() {
     // Call the /cohort route with a GET request
     const query = await fetch("http://localhost:3000/api/cohort");
@@ -14,16 +13,18 @@ export default async function Cohorts() {
   const cohorts = await GetCohorts();
 
   return (
-    <div className="mt-12 w-1/2 h-[calc(100%-10%)] grid grid-cols-1 sm:grid-cols-2 gap-3 overflow-y-scroll">
-      {cohorts.map((cohort) => (
-        <Link
-          key={cohort.id}
-          className="cohortsListItem"
-          href={`/dashboard/cohorts/${cohort.id}`}
-        >
-          {cohort.name}
-        </Link>
-      ))}
+    <div className="p-5 w-full h-full bg-grey-light">
+      <div className="w-1/2 mx-auto h-[calc(100%-5%)] grid grid-cols-1 sm:grid-cols-2 gap-3 overflow-y-scroll">
+        {cohorts.map((cohort) => (
+          <Link
+            key={cohort.id}
+            className="cohortsListItem"
+            href={`/dashboard/cohorts/${cohort.id}`}
+          >
+            {cohort.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
