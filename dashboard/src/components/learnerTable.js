311cba791fea75683cffea61731953693b482b67
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-25 22:20:58 +1200

    implemented sorting functionality for table columns by adding state variables, and a sort event to be triggered by clickng column header.

--- dashboard/src/components/learnerTable.js
+++ dashboard/src/components/learnerTable.js
@@ -6,7 +6,9 @@ import React, { useState } from "react";
 import React, { useState, useEffect } from "react";
 
 const Table = () => {
-  const [students, setStudents] = useState([]);
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortColumn, setSortColumn] = useState("");
+  const [learners, setlearners] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -14,7 +16,7 @@ const Table = () => {
       try {
         const response = await fetch("http://localhost:3000/api/learners");
         const data = await response.json();
-        setStudents(data.rows);
+        setlearners(data.rows);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -25,7 +27,53 @@ const Table = () => {
     fetchData();
   }, []);
 
-  const students = await GetStudents();
+  const handleSort = (column) => {
+    if (column === "startDate") {
+      if (startDateSortColumn === column) {
+        setStartDateSortOrder(startDateSortOrder === "asc" ? "desc" : "asc");
+      } else {
+        setStartDateSortColumn(column);
+        setStartDateSortOrder("asc");
+      }
+    } else {
+      if (sortColumn === column) {
+        setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+      } else {
+        setSortColumn(column);
+        setSortOrder("asc");
+      }
+    }
+  };
+
+  const sortedLearners = [...learners];
+  sortedLearners.sort((a, b) => {
+    if (sortColumn === "name") {
+      return sortOrder === "asc"
+        ? a.first_name.localeCompare(b.first_name)
+        : b.first_name.localeCompare(a.first_name);
+    } else if (sortColumn === "id") {
+      return sortOrder === "asc" ? a.id - b.id : b.id - a.id;
+    } else if (sortColumn === "cohort") {
+      return sortOrder === "asc"
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name);
+    } else if (sortColumn === "iQualify") {
+      return sortOrder === "asc"
+        ? a.last_updated.localeCompare(b.last_updated)
+        : b.last_updated.localeCompare(a.last_updated);
+    } else if (startDateSortColumn === "startDate") {
+      const dateA = new Date(a.programme_start);
+      const dateB = new Date(b.programme_start);
+
+      if (startDateSortOrder === "asc") {
+        return dateA - dateB;
+      } else {
+        return dateB - dateA;
+      }
+    }
+
+    return 0;
+  });
 
   return (
     <div className="flex flex-col overflow-x-auto">
@@ -33,72 +81,72 @@ const Table = () => {
       <div className="sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
           <div className="overflow-x-auto">
-          {loading ? (
+            {loading ? (
               <div>Loading...</div> // Show loading state
             ) : (
-            <table className="min-w-full text-left text-sm font-light">
-              <thead className="border-b font-medium dark:border-neutral-500">
-                <tr>
-                  <th scope="col" className="px-6 py-4">
-                    Name
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Id
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Cohort
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Start Date
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    End Date
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    iQualify(Last log in)
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Slack(Last active)
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    More
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {students.map((student) => (
-                  <tr
-                    className="border-b dark:border-neutral-500"
-                    key={student.id}
-                  >
-                    <td className="whitespace-nowrap px-6 py-4">
-                      {student.first_name + " " + student.last_name}
-                    </td>
-                    <td className="whitespace-nowrap px-6 py-4">
-                      {student.id}
-                    </td>
-                    <td className="whitespace-nowrap px-6 py-4">
-                      {student.name}
-                    </td>
-                    <td className="whitespace-nowrap px-6 py-4">
-                      {student.programme_start}
-                    </td>
-                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
-                    <td className="whitespace-nowrap px-6 py-4">
-                      {student.last_updated}
-                    </td>
-                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
-                    <td
-                      className="whitespace-nowrap px-6 py-4"
-                      onClick={() => alert("button clicked")}
-                    >
-                      →
-                    </td>
+              <table className="min-w-full text-left text-sm font-light">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                  <tr>
+                    <th scope="col" className="px-6 py-4">
+                      Name
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Id
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Cohort
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Start Date
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      End Date
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      iQualify(Last log in)
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Slack(Last active)
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      More
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-              )}
+                </thead>
+                <tbody>
+                  {learners.map((learner) => (
+                    <tr
+                      className="border-b dark:border-neutral-500"
+                      key={learner.id}
+                    >
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {learner.first_name + " " + learner.last_name}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {learner.id}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {learner.name}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {learner.programme_start}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {learner.last_updated}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                      <td
+                        className="whitespace-nowrap px-6 py-4"
+                        onClick={() => alert("button clicked")}
+                      >
+                        →
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
           </div>
         </div>
       </div>
