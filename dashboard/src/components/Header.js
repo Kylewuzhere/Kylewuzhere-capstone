16d656b362302c1ccad3110c4cdee6f9821a5f11
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2023-05-26 12:56:49 +1200

    add: useSession to retrieve session data; required false as only Logout button needs it; allows component to be rendered client side

--- dashboard/src/components/Header.js
+++ dashboard/src/components/Header.js
@@ -1,7 +1,13 @@
+"use client";
+
 import React from "react";
 import { LogoutButton } from "./logoutButton";
+import { useSession } from "next-auth/react";
 
-const Header = (session) => {
+const Header = () => {
+  const { data: session } = useSession({
+    required: false,
+  });
   return (
     <header className="fixed top-0 w-full h-12 bg-black text-white py-7 px-7 flex items-center justify-between">
       <img className="max-h-8" src="/images/logo-transparent.png" alt="Logo" />
