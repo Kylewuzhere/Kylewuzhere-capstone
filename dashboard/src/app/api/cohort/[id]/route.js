bebe6fd4a42ac38b697548217590435ac57f0cb6
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-31 11:21:11 +1200

    Added error handling

--- dashboard/src/app/api/cohort/[id]/route.js
+++ dashboard/src/app/api/cohort/[id]/route.js
@@ -5,10 +5,22 @@ export async function GET(request, { params }) {
   const id = params.id;
   const { searchParams } = request.nextUrl;
 
+  if (!id) {
+    return NextResponse.error(400, "Missing id");
+  }
+
   const query = `
     SELECT *
     FROM cohort
     JOIN users ON cohort.id = users.cohort_id
     WHERE cohort.id = $1;
   `;
+
+  try {
+    const { rows } = await pool.query(query, [id]);
+    return NextResponse.json({ rows });
+  } catch (error) {
+    console.error("Error fetching learner:", error);
+    return NextResponse.error(500, "Internal server error");
+  }
 }
