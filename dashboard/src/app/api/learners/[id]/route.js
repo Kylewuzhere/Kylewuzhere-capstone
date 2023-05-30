f20337a9393023ea98932ed76265e65cf42d86b2
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-30 12:07:32 +1200

    updated SQL query to include cohort name

--- dashboard/src/app/api/learners/[id]/route.js
+++ dashboard/src/app/api/learners/[id]/route.js
@@ -5,9 +5,13 @@ export async function GET(request, { params }) {
   const id = params.id;
   const { searchParams } = request.nextUrl;
   try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id,
-    ]);
+    const query = `
+      SELECT users.*, cohort.name
+      FROM users
+      FULL OUTER JOIN cohort ON users.cohort_id = cohort.id
+      WHERE users.id = $1
+    `;
+    const { rows } = await pool.query(query, [id]);
     return NextResponse.json({ rows });
   } catch (error) {
     console.error("Error fetching learner:", error);
