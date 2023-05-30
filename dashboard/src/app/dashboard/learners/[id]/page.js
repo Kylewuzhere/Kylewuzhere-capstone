9fb8c5f4c85ea8cdf9c270b5094343a570a90fa5
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-30 15:03:47 +1200

    imported learnerDetails component into individual learner page

--- dashboard/src/app/dashboard/learners/[id]/page.js
+++ dashboard/src/app/dashboard/learners/[id]/page.js
@@ -1,13 +1,19 @@
+"use client";
+
+import React from "react";
 import LearnersTitle from "@/components/LearnersTitle";
+import LearnerDetails from "@/components/LearnerDetails";
 
 export default function Learners({ params }) {
   return (
     <div className="w-full h-full bg-blue-light">
       <LearnersTitle />
-      <iframe
+      <LearnerDetails learnerId={params.id} />
+
+      {/* <iframe
         className="w-full h-full"
         src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/68130911-39a3-44d6-9510-49afa451eccf?directory_alias=diquicksights#p.userid=${params.id}`}
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 }
