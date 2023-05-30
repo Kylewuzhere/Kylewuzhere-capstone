8209b6b4de3bd7e256329c8e3b9737e7c905df05
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-31 11:20:03 +1200

    Added a SQL query which gets a cohort by id, and displays a list of learners from that cohort

--- dashboard/src/app/api/cohort/[id]/route.js
+++ dashboard/src/app/api/cohort/[id]/route.js
@@ -1,4 +1,14 @@
 import pool from "@/app/db";
 import { NextResponse } from "next/server";
 
-export async function GET(request, { params }) {}
+export async function GET(request, { params }) {
+  const id = params.id;
+  const { searchParams } = request.nextUrl;
+
+  const query = `
+    SELECT *
+    FROM cohort
+    JOIN users ON cohort.id = users.cohort_id
+    WHERE cohort.id = $1;
+  `;
+}
