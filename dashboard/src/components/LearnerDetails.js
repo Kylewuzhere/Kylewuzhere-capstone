ad41c09bf5f80ac3ec3533ffa5625293402e572b
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-30 15:43:36 +1200

    Added a more details button and a handler

--- dashboard/src/components/LearnerDetails.js
+++ dashboard/src/components/LearnerDetails.js
@@ -20,62 +20,74 @@ const LearnerDetails = ({ learnerId }) => {
   }, [learnerId]);
 
   const handleEmail = () => {
-    // Logic to handle email functionality
     console.log("Email learner:", learner.email);
   };
 
   const handleSlackMessage = () => {
-    // Logic to handle Slack message functionality
     console.log("Send Slack message to learner:", learner.first_name);
+  };
+
+  const handleMoreDetails = () => {
+    console.log("More details about the learner:", learner);
   };
 
   return (
     <div
       style={{ width: "500px" }}
-      className="bg-gray-200 rounded-md p-8 ml-4 mt-4"
+      className="bg-gray-200 rounded-md p-8 ml-4 mt-4 flex flex-col"
     >
       {learner && (
-        <div>
-          <div className="flex justify-between items-start mb-4">
-            <div>
-              <h2 className="text-xl font-bold mb-2">
-                {learner.first_name} {learner.last_name}
-              </h2>
-              <p className="mb-4">Cohort: {learner.name}</p>
-            </div>
-            <div className="flex flex-col items-end">
-              <div className="flex items-center space-x-2 mb-2">
-                <span className="text-gray-500 italic">Email</span>
-                <div>
-                  <button
-                    onClick={handleEmail}
-                    className="bg-transparent hover:bg-gray-300 rounded"
-                  >
-                    <Image
-                      src="/images/email.png"
-                      alt="Email"
-                      width={50}
-                      height={50}
-                    />
-                  </button>
-                </div>
+        <div className="flex justify-between">
+          <div>
+            <h2 className="text-xl font-bold mb-2">
+              {learner.first_name} {learner.last_name}
+            </h2>
+            <p className="mb-4">
+              <span className="text-gray-500">Cohort: </span>
+              {learner.name}
+            </p>
+            <button
+              onClick={handleMoreDetails}
+              className="bg-gray-300 hover:bg-gray-400 text-xs text-black px-4 py-2 rounded inline-flex items-center mt-4"
+            >
+              More Details
+              <span className="ml-2">&#9660;</span>
+            </button>
+          </div>
+          <div className="flex flex-col items-end">
+            <div className="flex items-center space-x-2 mb-2">
+              <span className="text-gray-500 italic text-sm">Email</span>
+              <div>
+                <button
+                  onClick={handleEmail}
+                  className="bg-transparent hover:bg-gray-300 rounded"
+                >
+                  <Image
+                    src="/images/email.png"
+                    alt="Email"
+                    width={50}
+                    height={50}
+                  />
+                </button>
               </div>
-              <div className="flex items-center space-x-2 text-sm">
-                <span className="text-gray-500 italic">Message on Slack</span>
-                <div>
-                  <button
-                    onClick={handleSlackMessage}
-                    className="bg-white hover:bg-gray-300 text-gray-800 rounded"
-                    disabled
-                  >
-                    <Image
-                      src="/images/slack.png"
-                      alt="Slack"
-                      width={50}
-                      height={50}
-                    />
-                  </button>
-                </div>
+            </div>
+            <div className="flex items-center space-x-2 text-sm">
+              <span className="text-gray-500 italic text-xs">
+                Message on Slack
+              </span>
+              <div>
+                <button
+                  onClick={handleSlackMessage}
+                  className="bg-white hover:bg-gray-300 text-gray-800 rounded"
+                  disabled
+                >
+                  <Image
+                    src="/images/slack.png"
+                    alt="Slack"
+                    width={50}
+                    height={50}
+                  />
+                </button>
               </div>
             </div>
           </div>
