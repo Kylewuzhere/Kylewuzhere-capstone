a20be4cbc9565b2454ca5d41827855206bcb46d8
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-30 18:25:59 +1200

    Added some basic code and cohorts title to the individual cohorts page, now when a cohort is clicked the user is taken to the individual cohort page instead of recieving a 404 error

---
+++ dashboard/src/app/dashboard/cohorts/[id]/page.js
@@ -0,0 +1,9 @@
+import CohortTitle from "@/components/CohortTitle";
+
+export default function Cohorts() {
+  return (
+    <div className=" w-full h-full bg-grey-light">
+      <CohortTitle />
+    </div>
+  );
+}
