b595a356bb04fd6fb134a939af9b13e6754d48b1
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-30 14:45:10 +1200

    Added functionality to fetch learner

--- dashboard/src/components/LearnerDetails.js
+++ dashboard/src/components/LearnerDetails.js
@@ -4,4 +4,18 @@ import React, { useEffect, useState } from "react";
 
 const LearnerDetails = ({ learnerId }) => {
   const [learner, setLearner] = useState(null);
+
+  useEffect(() => {
+    async function fetchLearner() {
+      try {
+        const response = await fetch(`/api/learners/${learnerId}`);
+        const data = await response.json();
+        setLearner(data.rows[0]);
+      } catch (error) {
+        console.error("Error fetching learner:", error);
+      }
+    }
+
+    fetchLearner();
+  }, [learnerId]);
 };
