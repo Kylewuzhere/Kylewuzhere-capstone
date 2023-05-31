d0f77790bf9a2c59d839c44b3b9e504296e56b97
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-31 17:08:51 +1200

    added table body, mapping over users state and rendering a row for each user

--- dashboard/src/components/CohortTable.js
+++ dashboard/src/components/CohortTable.js
@@ -27,6 +27,18 @@ const CohortTable = ({ cohortData }) => {
           <th>Github</th>
         </tr>
       </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{`${user.first_name} ${user.last_name}`}</td>
+            <td>{user.id}</td>
+            <td>{user.zoom_logged_in}</td>
+            <td>{user.iqualify_logged_in}</td>
+            <td>{user.slack_logged_in}</td>
+            <td>{user.github_last_commit}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
