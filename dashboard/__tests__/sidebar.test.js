60e7699fe8a8d529b658703ebd1c10dc8b8d9c26
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2023-06-01 10:37:47 +1200

    test: Sidebar button render and mock usePathname

---
+++ dashboard/__tests__/sidebar.test.js
@@ -0,0 +1,31 @@
+import { render } from "@testing-library/react";
+import Sidebar from "@/components/sidebar";
+import { usePathname } from "next/navigation";
+
+jest.mock("next/navigation");
+
+const navs = [
+  { text: "COHORTS", path: "/dashboard/cohorts" },
+  { text: "LEARNERS", path: "/dashboard/learners" },
+];
+
+describe("Sidebar", () => {
+  test.each(navs)("test $text button render", ({ text, path }) => {
+    usePathname.mockReturnValueOnce(path);
+    const { getByText } = render(<Sidebar />);
+
+    const button = getByText(text);
+    expect(button).toBeInTheDocument();
+  });
+  test.each(navs)(
+    `calls usePathname when $text button is clicked`,
+    ({ text, path }) => {
+      usePathname.mockReturnValueOnce(path);
+      const { getByText } = render(<Sidebar />);
+
+      const button = getByText(text);
+      button.click();
+      expect(usePathname).toHaveBeenCalled();
+    }
+  );
+});
