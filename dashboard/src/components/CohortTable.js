f80982ac900543fe20109597a4b5f274fd77d8f6
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-31 17:18:25 +1200

    Started styling. Added flexbox and some scrolling features, scrollbars will be shown if content overflows. Set background colour. Added margin

--- dashboard/src/components/CohortTable.js
+++ dashboard/src/components/CohortTable.js
@@ -16,30 +16,34 @@ const CohortTable = ({ cohortData }) => {
   }, [cohortData]);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>ID</th>
-          <th>Zoom</th>
-          <th>iQualify</th>
-          <th>Slack</th>
-          <th>Github</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => (
-          <tr key={user.id}>
-            <td>{`${user.first_name} ${user.last_name}`}</td>
-            <td>{user.id}</td>
-            <td>{user.zoom_logged_in}</td>
-            <td>{user.iqualify_logged_in}</td>
-            <td>{user.slack_logged_in}</td>
-            <td>{user.github_last_commit}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="flex flex-col overflow-x-auto bg-grey-light">
+      <div className="-mx-6 lg:-mx-8">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>ID</th>
+              <th>Zoom</th>
+              <th>iQualify</th>
+              <th>Slack</th>
+              <th>Github</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{`${user.first_name} ${user.last_name}`}</td>
+                <td>{user.id}</td>
+                <td>{user.zoom_logged_in}</td>
+                <td>{user.iqualify_logged_in}</td>
+                <td>{user.slack_logged_in}</td>
+                <td>{user.github_last_commit}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
