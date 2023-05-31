b1086516b9caa7af5b8596a6101c089c258f9866
KobeVasquezDI <86027430+KobeVasquezDI@users.noreply.github.com>
2023-06-01 09:24:37 +1200

    added functioning search bar

--- dashboard/src/components/SearchBar.js
+++ dashboard/src/components/SearchBar.js
@@ -1,20 +1,37 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+// SearchBar is a client component that lets users search for learners by name.
+// upon submission, the searchbar will set a search query with the name "search"
+// formatted as: currentURL + ?search= + input-value
 
 export default function SearchBar() {
+  const [input, setInput] = useState("");
+
+  // replaces all non-alphabetic characters with an empty string, unless it's a space
+  const handleChange = (event) => {
+    const result = event.target.value.replace(/[^a-z ]/gi, "");
+
+    setInput(result);
+  };
+
   return (
-    <div className="search-bar">
-      <form role="search">
-        <div>
-          <input
-            type="search"
-            id="mysearch"
-            placeholder="Search Name"
-            name="q"
-          />
-          <button>Search</button>
-        </div>
+    <div className="flex justify-end mr-10 mt-5">
+      <form role="search" className="flex">
+        <button
+          type="submit"
+          className="bg-white px-4 py-1.5 border border-grey-dark border-r-0 rounded-l-lg focus:outline-none"
+        >
+          🔎
+        </button>
+        <input
+          type="search"
+          id="mysearch"
+          placeholder="Search Learner Name"
+          name="search"
+          value={input}
+          onChange={handleChange}
+          className="px-4 py-1.5 bg-blue-500 border border-grey-dark border-l-0 font-semibold rounded-r-lg focus:outline-none"
+        />
       </form>
     </div>
   );
