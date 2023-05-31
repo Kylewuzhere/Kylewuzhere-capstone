3ba8f609f9c5253bd29772c2d4ed52be8d0eed2d
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-05-31 17:21:06 +1200

    Added padding and min-widths,and  some changes for different sized screens

--- dashboard/src/components/CohortTable.js
+++ dashboard/src/components/CohortTable.js
@@ -18,30 +18,34 @@ const CohortTable = ({ cohortData }) => {
   return (
     <div className="flex flex-col overflow-x-auto bg-grey-light">
       <div className="-mx-6 lg:-mx-8">
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>ID</th>
-              <th>Zoom</th>
-              <th>iQualify</th>
-              <th>Slack</th>
-              <th>Github</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td>{`${user.first_name} ${user.last_name}`}</td>
-                <td>{user.id}</td>
-                <td>{user.zoom_logged_in}</td>
-                <td>{user.iqualify_logged_in}</td>
-                <td>{user.slack_logged_in}</td>
-                <td>{user.github_last_commit}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div className="overflow-x-auto">
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>ID</th>
+                  <th>Zoom</th>
+                  <th>iQualify</th>
+                  <th>Slack</th>
+                  <th>Github</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{`${user.first_name} ${user.last_name}`}</td>
+                    <td>{user.id}</td>
+                    <td>{user.zoom_logged_in}</td>
+                    <td>{user.iqualify_logged_in}</td>
+                    <td>{user.slack_logged_in}</td>
+                    <td>{user.github_last_commit}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
