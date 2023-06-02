1b511394c259c70135ebbc6cb84ffbed93e65723
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-02 13:27:12 +1200

    imported new dependency for links

--- dashboard/src/components/CohortTable.js
+++ dashboard/src/components/CohortTable.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
 const CohortTable = ({ cohortData }) => {
   const [users, setUsers] = useState([]);
