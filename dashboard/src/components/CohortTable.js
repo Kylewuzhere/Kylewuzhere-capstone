9a9d27c4edf8e3c1f421406d02cf5435b19d7b90
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-02 16:17:33 +1200

    removed deadspace in code

--- dashboard/src/components/CohortTable.js
+++ dashboard/src/components/CohortTable.js
@@ -9,7 +9,7 @@ const CohortTable = ({ cohortData }) => {
     async function fetchData() {
       const response = await fetch(
         `http://localhost:3000/api/cohort/${cohortData}`,
-        { cache: "no-store" }
+        { cache: 'no-store'}
       );
       const data = await response.json();
       setUsers(data.rows);
@@ -49,7 +49,6 @@ const CohortTable = ({ cohortData }) => {
                     <td className="whitespace-nowrap px-6 py-4">
                       {`${user.first_name} ${user.last_name}`}
                     </td>
-
                     <td className="whitespace-nowrap px-6 py-4">
                       {user.zoom_logged_in}
                     </td>
