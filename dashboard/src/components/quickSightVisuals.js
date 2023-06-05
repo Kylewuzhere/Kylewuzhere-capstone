25318d0b0c3c8f704f07fd9bf92552b99fffe596
Jason Zhong <jasonzhong@developersinstitute.co.nz>
2023-06-05 17:24:12 +1200

    embed visuals

--- dashboard/src/components/quickSightVisuals.js
+++ dashboard/src/components/quickSightVisuals.js
@@ -7,34 +7,32 @@ const IframeVisuals = ({ data }) => {
       </div>
       <iframe
         className="w-full h-full  gap-2 "
-        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/2e473c8d-320b-468f-bdb1-dca1b9478c7d/sheets/2e473c8d-320b-468f-bdb1-dca1b9478c7d_af514f0a-3519-4929-b540-cba36abb1c22/visuals/2e473c8d-320b-468f-bdb1-dca1b9478c7d_be455211-b274-4c13-bef8-7eb06525e7b4?directory_alias=diquicksights#p.userId=${data}`}
+        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/195adde3-34b4-47da-afd4-b9388c8379ca/sheets/195adde3-34b4-47da-afd4-b9388c8379ca_f61a5de5-dcb9-4886-b36c-ada9178c7b3c/visuals/195adde3-34b4-47da-afd4-b9388c8379ca_fe663a82-5534-4b12-b11e-dd02ce0d9bf5?directory_alias=diquicksights#p.userId=${data}`}
       ></iframe>
       <iframe
         className="w-full h-full col-span-2 "
-        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/80205ca8-55f3-4fcc-be08-500b3cf898da/sheets/80205ca8-55f3-4fcc-be08-500b3cf898da_845fffbd-b579-4556-bcb6-0ede0eb03537/visuals/80205ca8-55f3-4fcc-be08-500b3cf898da_adaee9c9-d8ad-4c7c-ba7b-2b2fc52c5300?directory_alias=diquicksights#p.userId=${data}`}
+        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/bbc91b5d-9c57-4ae9-8c29-638f3b446a61/sheets/bbc91b5d-9c57-4ae9-8c29-638f3b446a61_6d48a48d-8d80-4419-a824-e25ea982b38e/visuals/bbc91b5d-9c57-4ae9-8c29-638f3b446a61_961d7319-e8ea-407a-b98a-be20b46e0cc1?directory_alias=diquicksights#p.userId=${data}`}
       ></iframe>
-
       <iframe
         className="w-full  h-full col-span-3 row-span-4 "
-        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/24993a92-0bb3-4e0a-8e28-4064538f5ee3/sheets/24993a92-0bb3-4e0a-8e28-4064538f5ee3_50d891ca-ca91-4503-9c9b-3743ac37e02d/visuals/24993a92-0bb3-4e0a-8e28-4064538f5ee3_e9c08be1-ed82-46a7-b1cd-8690d36924f8?directory_alias=diquicksights#p.userId=${data}`}
-      />
-
-      <iframe
-        className="w-full  h-full"
-        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/65962c2b-6f72-4f8a-a732-e2535f624430/sheets/65962c2b-6f72-4f8a-a732-e2535f624430_240fdd4f-654f-456a-8c66-c3080c391e26/visuals/65962c2b-6f72-4f8a-a732-e2535f624430_2f5e3618-e951-42a3-87b8-8e1baa7ad18f?directory_alias=diquicksights#p.userId=${data}`}
+        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/72e0c104-bab0-4b5c-b6cc-0c6685c7eb82/sheets/72e0c104-bab0-4b5c-b6cc-0c6685c7eb82_3dac6cc4-2c5b-4b5f-9b21-d240f9e3e0de/visuals/72e0c104-bab0-4b5c-b6cc-0c6685c7eb82_96c355b6-9d60-4772-ae56-4879700aa3ad?directory_alias=diquicksights#p.userId=${data}`}
       />
       <iframe
         className="w-full  h-full"
-        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/65962c2b-6f72-4f8a-a732-e2535f624430/sheets/65962c2b-6f72-4f8a-a732-e2535f624430_240fdd4f-654f-456a-8c66-c3080c391e26/visuals/65962c2b-6f72-4f8a-a732-e2535f624430_cac44aa0-0598-46f8-85e6-e34e9a182f5a?directory_alias=diquicksights#p.userId=${data}`}
+        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/cf173d36-4505-44d0-b82c-69ac99884804/sheets/cf173d36-4505-44d0-b82c-69ac99884804_d9b02ac5-1128-48cf-9411-85f76b02fb5d/visuals/cf173d36-4505-44d0-b82c-69ac99884804_f76f6d3d-8004-4ab2-9ca3-562cdd0ee8af?directory_alias=diquicksights#p.userId=${data}`}
       />
       <iframe
         className="w-full  h-full"
-        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/fe95c68b-34bc-423d-9b73-e898e7b755fa/sheets/fe95c68b-34bc-423d-9b73-e898e7b755fa_beef78e7-93c6-4cd7-b05e-fed5178c6d66/visuals/fe95c68b-34bc-423d-9b73-e898e7b755fa_5a03b64f-b21c-4d42-8417-0e6a119946bb?directory_alias=diquicksights#p.userId=${data}`}
+        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/cf173d36-4505-44d0-b82c-69ac99884804/sheets/cf173d36-4505-44d0-b82c-69ac99884804_d9b02ac5-1128-48cf-9411-85f76b02fb5d/visuals/cf173d36-4505-44d0-b82c-69ac99884804_3347468f-06d2-470d-907d-fbcb48c6937c?directory_alias=diquicksights#p.userId=${data}`}
+      />
+      <iframe
+        className="w-full  h-full"
+        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/72c24fab-ff00-426e-85be-30efa0bd8483/sheets/72c24fab-ff00-426e-85be-30efa0bd8483_b2dc917e-d8c1-44c7-ab9e-d849f84352e7/visuals/72c24fab-ff00-426e-85be-30efa0bd8483_70f5c827-3352-40b4-87da-f3a75c2b4592?directory_alias=diquicksights#p.userId=${data}`}
       />
 
       <iframe
         className="w-full  h-full col-span-3 row-span-2"
-        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/fe95c68b-34bc-423d-9b73-e898e7b755fa/sheets/fe95c68b-34bc-423d-9b73-e898e7b755fa_beef78e7-93c6-4cd7-b05e-fed5178c6d66/visuals/fe95c68b-34bc-423d-9b73-e898e7b755fa_6ca1f822-76c7-41dc-97b0-643589df59cf?directory_alias=diquicksights#p.userId=${data}`}
+        src={`https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/236143717967/dashboards/72c24fab-ff00-426e-85be-30efa0bd8483/sheets/72c24fab-ff00-426e-85be-30efa0bd8483_b2dc917e-d8c1-44c7-ab9e-d849f84352e7/visuals/72c24fab-ff00-426e-85be-30efa0bd8483_01502591-d32a-49c5-86c6-e2bb237f4194?directory_alias=diquicksights#p.userId=${data}`}
       />
     </div>
   );
