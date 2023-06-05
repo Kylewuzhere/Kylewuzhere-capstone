68984b27644a2e13b2ddf91b49ad3c7da000e4ac
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-06 11:12:30 +1200

    imported Cohort details into the individual Cohort page and changed the route to bring in data for the Cohort name

--- dashboard/src/app/dashboard/cohorts/[id]/page.js
+++ dashboard/src/app/dashboard/cohorts/[id]/page.js
@@ -1,10 +1,14 @@
 import CohortTitle from "@/components/CohortTitle";
+import CohortDetails from "@/components/CohortDetails";
 import CohortTable from "@/components/CohortTable";
 
 export default function Cohort({ params }) {
+  const cohortId = String(params.id);
+
   return (
     <div className="w-full h-full relative bg-grey-light">
       <CohortTitle />
+      <CohortDetails cohortId={cohortId} />
       <div className="absolute inset-x-0 bottom-0 h-1/2 overflow-auto">
         <CohortTable cohortData={params.id} />
       </div>
