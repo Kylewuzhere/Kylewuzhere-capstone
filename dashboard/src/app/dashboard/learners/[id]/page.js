6bf0f67b81b8aa3e79a110cce5b5f5d0499d2674
Jason Zhong <jasonzhong@developersinstitute.co.nz>
2023-06-06 13:46:33 +1200

    update file name to inline with other files

--- dashboard/src/app/dashboard/learners/[id]/page.js
+++ dashboard/src/app/dashboard/learners/[id]/page.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import LearnersTitle from "@/components/LearnersTitle";
-import IframeVisuals from "@/components/quickSightVisuals";
+import IframeVisuals from "@/components/QuickSightVisuals";
 
 export default function Learners({ params }) {
   return (
