b4684f5195c60b7bf5761ba6fd5857f561964932
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-06 13:13:04 +1200

    reverted unused code in sql query

--- dashboard/src/app/api/cohort/[id]/route.js
+++ dashboard/src/app/api/cohort/[id]/route.js
@@ -15,38 +15,34 @@ export async function GET(request, { params }) {
   COALESCE(zoom.event_time::text, NULL) AS zoom_logged_in,
   COALESCE(iqualify.event_time::text, NULL) AS iqualify_logged_in,
   COALESCE(slack.event_time::text, NULL) AS slack_logged_in,
-  COALESCE(github.event_time::text, NULL) AS github_last_commit,
-  cohorts.name AS cohort_name
+  COALESCE(github.event_time::text, NULL) AS github_last_commit
 FROM
-  learners
-  LEFT JOIN (
-    SELECT learner_id, MAX(event_time) AS event_time
-    FROM activity_log
-    WHERE source = 'zoom' AND event_type = 'joined meeting'
-    GROUP BY learner_id
-  ) AS zoom ON learners.id = zoom.learner_id 
-  LEFT JOIN (
-    SELECT learner_id, MAX(event_time) AS event_time
-    FROM activity_log
-    WHERE source = 'iqualify' AND (event_type = 'logged in' OR event_type = 'joined meeting')
-    GROUP BY learner_id
-  ) AS iqualify ON learners.id = iqualify.learner_id
-  LEFT JOIN (
-    SELECT learner_id, MAX(event_time) AS event_time
-    FROM activity_log
-    WHERE source = 'slack' AND event_type = 'logged in'
-    GROUP BY learner_id
-  ) AS slack ON learners.id = slack.learner_id 
-  LEFT JOIN (
-    SELECT learner_id, MAX(event_time) AS event_time
-    FROM activity_log
-    WHERE source = 'github' AND event_type = 'pushed commit'
-    GROUP BY learner_id
-  ) AS github ON learners.id = github.learner_id
-  LEFT JOIN cohorts ON learners.cohort_id = cohorts.id
-WHERE
-  learners.cohort_id = $1;
-;
+learners
+LEFT JOIN (
+  SELECT learner_id, MAX(event_time) AS event_time
+  FROM activity_log
+  WHERE source = 'zoom' AND event_type = 'joined meeting'
+  GROUP BY learner_id
+) AS zoom ON learners.id = zoom.learner_id 
+LEFT JOIN (
+  SELECT learner_id, MAX(event_time) AS event_time
+  FROM activity_log
+  WHERE source = 'iqualify' AND (event_type = 'logged in' OR event_type = 'joined meeting')
+  GROUP BY learner_id
+) AS iqualify ON learners.id = iqualify.learner_id
+LEFT JOIN (
+  SELECT learner_id, MAX(event_time) AS event_time
+  FROM activity_log
+  WHERE source = 'slack' AND event_type = 'logged in'
+  GROUP BY learner_id
+) AS slack ON learners.id = slack.learner_id 
+LEFT JOIN (
+  SELECT learner_id, MAX(event_time) AS event_time
+  FROM activity_log
+  WHERE source = 'github' AND event_type = 'pushed commit'
+  GROUP BY learner_id
+) AS github ON learners.id = github.learner_id
+WHERE learners.cohort_id = $1;
 
 `;
 
