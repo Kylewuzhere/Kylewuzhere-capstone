a36a04d56f5153b5af18fec6fb1914913ff4652c
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-06 12:50:26 +1200

    Added state, set initial state and added error handling state

--- dashboard/src/components/CohortDetails.js
+++ dashboard/src/components/CohortDetails.js
@@ -1,4 +1,8 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-const CohortDetails = ({ cohortId }) => {};
+const CohortDetails = ({ cohortId }) => {
+    const [cohort, setCohort] = useState(null);
+    const [error, setError] = useState(null);
+    
+  };
