2b4275635fec8505559e63a97e1107c3b5f062f4
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-07 14:56:39 +1200

    Used onFilterChange  passing the selected filter value. This function will be provided as a prop and will handle the filter change event

--- dashboard/src/components/LearnersTitle.js
+++ dashboard/src/components/LearnersTitle.js
@@ -1,4 +1,8 @@
 const LearnersTitle = ({ selectedFilter, onFilterChange }) => {
+  const handleFilter = (filter) => {
+    onFilterChange(filter);
+  };
+
   return (
     <div className="w-full h-12  bg-blue text-black text-3xl font-semibold pt-11 pb-9 px-12 flex items-center justify-between">
       <h1 className="ml-4">LEARNERS</h1>
