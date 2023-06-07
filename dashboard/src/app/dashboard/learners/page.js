b091486d5b90d025e8bbe357e8fba5204bb496a9
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-07 14:42:41 +1200

    updated the learners page to pass state from the button on learner title to the learner content component as props

--- dashboard/src/app/dashboard/learners/page.js
+++ dashboard/src/app/dashboard/learners/page.js
@@ -4,12 +4,19 @@ import LearnersTitle from "@/components/LearnersTitle";
 import LearnerContent from "@/components/LearnerContent";
 
 export default function Learners() {
-  const [filter, setFilter] = useState("all");
+  const [selectedFilter, setSelectedFilter] = useState("all");
+
+  const handleFilterChange = (filter) => {
+    setSelectedFilter(filter);
+  };
 
   return (
     <div className="w-full h-full border-b-2 border-black bg-blue-light">
-      <LearnersTitle filter={filter} setFilter={setFilter} />
-      <LearnerContent filter={filter} />
+      <LearnersTitle
+        selectedFilter={selectedFilter}
+        onFilterChange={handleFilterChange}
+      />
+      <LearnerContent selectedFilter={selectedFilter} />
     </div>
   );
 }
