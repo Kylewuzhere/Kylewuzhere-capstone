d7f60a1e343ac2a1e0d42a416b836c5342a96192
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-08 11:32:22 +1200

    Added logic to parse the date as it comes in as a string. We need it to be in date format to apply logic for the filters

--- dashboard/src/app/dashboard/cohorts/page.js
+++ dashboard/src/app/dashboard/cohorts/page.js
@@ -21,6 +21,11 @@ export default function Cohorts() {
     filterCohorts();
   }, [selectedFilter]);
 
+  const parseDate = (dateString) => {
+    const [day, month, year] = dateString.split("/");
+    return new Date(`${year}-${month}-${day}`);
+  };
+
   return (
     <div className=" w-full h-full bg-grey-light">
       <div className="w-full mb-2 border-b-2 border-black ">
