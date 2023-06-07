478843471ebc89dade8a7d92890d2905c46289cf
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-07 15:04:32 +1200

    added dependencies back in to LearnerTitle, added use client to allow state to be used

--- dashboard/src/components/LearnersTitle.js
+++ dashboard/src/components/LearnersTitle.js
@@ -1,10 +1,13 @@
+"use client";
+import React from "react";
+
 const LearnersTitle = ({ selectedFilter, onFilterChange }) => {
   const handleFilter = (filter) => {
     onFilterChange(filter);
   };
 
   return (
-    <div className="w-full h-12  bg-blue text-black text-3xl font-semibold pt-11 pb-9 px-12 flex items-center justify-between">
+    <div className="w-full h-12 bg-blue text-black text-3xl font-semibold pt-11 pb-9 px-12 flex items-center justify-between">
       <h1 className="ml-4">LEARNERS</h1>
       <div className="text-sm flex space-x-4 mr-4">
         <button
