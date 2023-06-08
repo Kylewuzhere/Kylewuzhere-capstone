2bceeb5778cc294786ee68698e7eb1d383a226f9
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-08 20:51:16 +1200

    Update the search term in the parent component

--- dashboard/src/components/SearchBar.js
+++ dashboard/src/components/SearchBar.js
@@ -1,23 +1,18 @@
 "use client";
 import React, { useState } from "react";
-// SearchBar is a client component that lets users search for learners by name.
-// upon submission, the searchbar will set a search query with the name "search"
-// formatted as: currentURL + ?search= + input-value
 
-export default function SearchBar() {
+export default function SearchBar({ setSearch }) {
   const [input, setInput] = useState("");
 
-  // replaces all non-alphabetic characters with an empty string, unless it's a space
-  // ref: https://bobbyhadz.com/blog/react-input-only-letters
   const handleChange = (event) => {
     const result = event.target.value.replace(/[^a-z\p{L} ]/giu, "");
-
     setInput(result);
+    setSearch(result); 
   };
 
   return (
     <div className="flex justify-end mr-10 mt-5">
-      <form role="search" className="flex">
+      <form role="search" className="flex" onSubmit={(e) => e.preventDefault()}>
         <button
           type="submit"
           className="bg-white px-4 py-1.5 border border-grey-dark border-r-0 rounded-l-lg focus:outline-none"
