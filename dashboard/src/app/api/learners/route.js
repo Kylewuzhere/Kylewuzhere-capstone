129110634deacd7a06d0b895eb04f0575676ab8f
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2023-06-08 14:12:08 +1200

    add: optimize queries and add offset and limit to implement pagination

--- dashboard/src/app/api/learners/route.js
+++ dashboard/src/app/api/learners/route.js
@@ -9,6 +9,11 @@ export async function GET(request) {
 
   // query is the search query from the url with the name "search"
   const query = searchParams.get("search");
+  const limit = searchParams.get("limit");
+  const page = searchParams.get("page");
+  const offset = limit * (page - 1);
+
+  console.log(query, limit, page, offset);
 
   if (query !== null && query !== "" && query !== " ") {
     // slicer takes a string, returns an array of strings ready for SQL query
@@ -28,20 +33,25 @@ export async function GET(request) {
     // retrieves all learners that match the search query (NAME)
     const { rows } = await pool.query(
       `SELECT
-          learners.id,
-          learners.first_name,
-          learners.last_name,
-          learners.current_subject_id,
-          cohorts.name AS cohort_name,
-          cohorts.programme_start,
-          learners.programme,
-          (SELECT MAX(event_time) FROM activity_log WHERE source = 'zoom' AND learner_id = learners.id) AS zoom_logged_in,
-          (SELECT MAX(event_time) FROM activity_log WHERE source = 'iqualify' AND learner_id = learners.id) AS iqualify_logged_in,
-          (SELECT MAX(event_time) FROM activity_log WHERE source = 'slack' AND learner_id = learners.id) AS slack_logged_in,
-          (SELECT MAX(event_time) FROM activity_log WHERE source = 'github' AND learner_id = learners.id) AS github_last_commit
-        FROM learners
-        JOIN cohorts ON learners.cohort_id = cohorts.id
-        WHERE (learners.first_name LIKE $1 OR learners.last_name LIKE $1) OR (learners.first_name LIKE $2 OR learners.last_name LIKE $2)`,
+        learners.id,
+        learners.first_name,
+        learners.last_name,
+        learners.current_subject_id,
+        cohorts.name AS cohort_name,
+        cohorts.programme_start,
+        learners.programme,
+      MAX(CASE WHEN activity_log.source = 'zoom' THEN activity_log.event_time END) AS zoom_logged_in,
+      MAX(CASE WHEN activity_log.source = 'iqualify' THEN activity_log.event_time END) AS iqualify_logged_in,
+      MAX(CASE WHEN activity_log.source = 'slack' THEN activity_log.event_time END) AS slack_logged_in,
+      MAX(CASE WHEN activity_log.source = 'github' THEN activity_log.event_time END) AS github_last_commit
+      FROM learners
+      JOIN cohorts ON learners.cohort_id = cohorts.id
+      LEFT JOIN activity_log ON learners.id = activity_log.learner_id
+      WHERE learners.first_name LIKE $1 OR learners.last_name LIKE $1 OR learners.first_name LIKE $2 OR learners.last_name LIKE $2 
+      GROUP BY learners.id, learners.first_name, learners.last_name, learners.current_subject_id, cohorts.name, cohorts.programme_start, learners.programme
+      ORDER BY learners.first_name
+      LIMIT ${limit}
+      OFFSET ${offset}`,
       [entries[0], entries[1]]
     );
     return NextResponse.json({ rows });
@@ -49,19 +59,24 @@ export async function GET(request) {
     // returns ALL learners
     const { rows } = await pool.query(
       `SELECT
-          learners.id,
-          learners.first_name,
-          learners.last_name,
-          learners.current_subject_id,
-          cohorts.name AS cohort_name,
-          cohorts.programme_start,
-          learners.programme,
-          (SELECT MAX(event_time) FROM activity_log WHERE source = 'zoom' AND learner_id = learners.id) AS zoom_logged_in,
-          (SELECT MAX(event_time) FROM activity_log WHERE source = 'iqualify' AND learner_id = learners.id) AS iqualify_logged_in,
-          (SELECT MAX(event_time) FROM activity_log WHERE source = 'slack' AND learner_id = learners.id) AS slack_logged_in,
-          (SELECT MAX(event_time) FROM activity_log WHERE source = 'github' AND learner_id = learners.id) AS github_last_commit
-        FROM learners
-        JOIN cohorts ON learners.cohort_id = cohorts.id`
+        learners.id,
+        learners.first_name,
+        learners.last_name,
+        learners.current_subject_id,
+        cohorts.name AS cohort_name,
+        cohorts.programme_start,
+        learners.programme,
+      MAX(CASE WHEN activity_log.source = 'zoom' THEN activity_log.event_time END) AS zoom_logged_in,
+      MAX(CASE WHEN activity_log.source = 'iqualify' THEN activity_log.event_time END) AS iqualify_logged_in,
+      MAX(CASE WHEN activity_log.source = 'slack' THEN activity_log.event_time END) AS slack_logged_in,
+      MAX(CASE WHEN activity_log.source = 'github' THEN activity_log.event_time END) AS github_last_commit
+      FROM learners
+      JOIN cohorts ON learners.cohort_id = cohorts.id
+      LEFT JOIN activity_log ON learners.id = activity_log.learner_id
+      GROUP BY learners.id, learners.first_name, learners.last_name, learners.current_subject_id, cohorts.name, cohorts.programme_start, learners.programme
+      ORDER BY learners.first_name
+      LIMIT ${limit}
+      OFFSET ${offset}`
     );
     return NextResponse.json({ rows });
   }
