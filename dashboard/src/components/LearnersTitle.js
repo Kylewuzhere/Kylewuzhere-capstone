e817e4c416203f67e24ef305fdbc6591ab5c26b9
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-09 10:32:05 +1200

    Refactored Cohort filter duplicate code as well

--- dashboard/src/components/LearnersTitle.js
+++ dashboard/src/components/LearnersTitle.js
@@ -1,6 +1,3 @@
-"use client";
-import React from "react";
-
 const FilterButton = ({ filter, selectedFilter, onFilterChange }) => {
   const isSelected = selectedFilter === filter;
   const buttonClass = `rounded ${
