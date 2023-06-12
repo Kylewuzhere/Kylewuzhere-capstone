4e4903754fa2dd7c196981d22bb6d16749942b61
KobeVasquezDI <86027430+KobeVasquezDI@users.noreply.github.com>
2023-06-12 23:37:16 +1200

    added response codes to api/learners/[id]

--- dashboard/src/app/api/learners/[id]/route.js
+++ dashboard/src/app/api/learners/[id]/route.js
@@ -1,21 +1,33 @@
 import pool from "@/app/db";
 import { NextResponse } from "next/server";
+import formatChecker from "@/utils/formatChecker";
 
 export async function GET(request, { params }) {
   const id = params.id;
-
-  try {
-    const query = `
+  const query = `
     SELECT learners.*, cohorts.name, subjects.name AS subject_name
     FROM learners
     FULL OUTER JOIN cohorts ON learners.cohort_id = cohorts.id
     JOIN subjects ON learners.current_subject_id = subjects.id
     WHERE learners.id = $1;
     `;
+
+  try {
+    const checkedId = formatChecker(id);
     const { rows } = await pool.query(query, [id]);
-    return NextResponse.json({ rows });
+    if (rows.length < 1 || !checkedId) {
+      return NextResponse.json(
+        {
+          error: "Learner Not Found",
+          rows,
+        },
+        { status: 404 }
+      );
+    } else {
+      return NextResponse.json({ rows }, { status: 200 });
+    }
   } catch (error) {
     console.error("Error fetching learner:", error);
-    return null;
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
