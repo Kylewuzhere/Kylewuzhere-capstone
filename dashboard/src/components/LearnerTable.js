6ce25406e9f83a81000d33ad758096b4251d8fc7
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2023-06-12 14:45:58 +1200

    style: remove nested divs and overflow-x

--- dashboard/src/components/LearnerTable.js
+++ dashboard/src/components/LearnerTable.js
@@ -59,105 +59,98 @@ const LearnerTable = ({ content }) => {
   });
 
   return (
-    <div className="flex flex-col overflow-x-auto">
-      <div className="sm:-mx-6 lg:-mx-8">
-        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-          <div className="overflow-x-auto">
-            {content.length === 0 ? (
-              <p className="mx-5 my-5">No Learners Found</p>
-            ) : (
-              <table className="min-w-full text-left text-sm font-light">
-                <thead className="border-b font-medium dark:border-neutral-500">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 cursor-pointer"
-                      onClick={() => handleSort("name")}
-                    >
-                      Name {getSortIcon("name")}
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 cursor-pointer"
-                      onClick={() => handleSort("cohort")}
-                    >
-                      Cohort {getSortIcon("cohort")}
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 cursor-pointer"
-                      onClick={() => handleSort("startDate")}
-                    >
-                      Start Date {getSortIcon("startDate")}
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 cursor-pointer"
-                      onClick={() => handleSort("programmeLevel")}
-                    >
-                      Programme Level {getSortIcon("programmeLevel")}
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 cursor-pointer"
-                      onClick={() => handleSort("iQualify")}
-                    >
-                      iQualify (Last log in) {getSortIcon("iQualify")}
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 cursor-pointer"
-                      onClick={() => handleSort("slack")}
-                    >
-                      Slack (Last active) {getSortIcon("slack")}
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      More
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {sortedLearners.map((learner) => (
-                    <tr
-                      className="border-b dark:border-neutral-500"
-                      key={learner.id}
-                    >
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {learner.first_name + " " + learner.last_name}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {learner.cohort_name}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {learner.programme_start}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {learner.programme}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {learner.iqualify_logged_in}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {learner.slack_logged_in || "N/A"}
-                      </td>
-                      <td className="cursor-pointer hover:bg-gray-200 rounded">
-                        <Link
-                          className="w-full h-full"
-                          href={`/dashboard/learners/${learner.id}`}
-                        >
-                          <div className="px-6 py-4  flex items-left">
-                            <span className="text-[14px]">&#9658;</span>
-                          </div>
-                        </Link>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
-          </div>
+    <div>
+      {content.length === 0 ? (
+        <div>
+          <p className="mx-5 my-5">No Learners Found</p>
         </div>
-      </div>
+      ) : (
+        <table className="min-w-full text-left text-sm font-light">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th
+                scope="col"
+                className="px-6 py-4 cursor-pointer"
+                onClick={() => handleSort("name")}
+              >
+                Name {getSortIcon("name")}
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 cursor-pointer"
+                onClick={() => handleSort("cohort")}
+              >
+                Cohort {getSortIcon("cohort")}
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 cursor-pointer"
+                onClick={() => handleSort("startDate")}
+              >
+                Start Date {getSortIcon("startDate")}
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 cursor-pointer"
+                onClick={() => handleSort("programmeLevel")}
+              >
+                Programme Level {getSortIcon("programmeLevel")}
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 cursor-pointer"
+                onClick={() => handleSort("iQualify")}
+              >
+                iQualify (Last log in) {getSortIcon("iQualify")}
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 cursor-pointer"
+                onClick={() => handleSort("slack")}
+              >
+                Slack (Last active) {getSortIcon("slack")}
+              </th>
+              <th scope="col" className="px-6 py-4">
+                More
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedLearners.map((learner) => (
+              <tr className="border-b dark:border-neutral-500" key={learner.id}>
+                <td className="whitespace-nowrap px-6 py-4">
+                  {learner.first_name + " " + learner.last_name}
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  {learner.cohort_name}
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  {learner.programme_start}
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  {learner.programme}
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  {learner.iqualify_logged_in}
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  {learner.slack_logged_in || "N/A"}
+                </td>
+                <td className="cursor-pointer hover:bg-gray-200 rounded">
+                  <Link
+                    className="w-full h-full"
+                    href={`/dashboard/learners/${learner.id}`}
+                  >
+                    <div className="px-6 py-4  flex items-left">
+                      <span className="text-[14px]">&#9658;</span>
+                    </div>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
