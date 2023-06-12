15c3d8adb085fdf10a5003ff94901edc3efbcc1f
KobeVasquezDI <86027430+KobeVasquezDI@users.noreply.github.com>
2023-06-12 23:49:04 +1200

    formatChecker.js for cohortId & learnersId added to utils folder

---
+++ dashboard/src/utils/formatChecker.js
@@ -0,0 +1,9 @@
+const formatChecker = (input) => {
+  const regex = new RegExp(
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
+  );
+  const checked = regex.test(input);
+  return checked;
+};
+
+export default formatChecker;
