47c9e9f3208e5fe95f14ec5c7f58e51f808b781d
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-12 20:51:20 +1200

    Added parameter values to queryParameters array: After constructing the SQL query, the code checks if search has a value. If it does, the value % is pushed to the queryParameters array. This ensures that the user input is treated as a parameter instead of being directly included in the SQL query.

--- dashboard/src/app/api/learners/route.js
+++ dashboard/src/app/api/learners/route.js
@@ -49,7 +49,6 @@ export async function GET(request) {
       LEFT JOIN activity_log ON learners.id = activity_log.learner_id`;
 
   const queryParameters = [];
-
   if (filter === "inactive") {
     sqlQuery += ` WHERE ${filters[filter]}`;
   } else if (filter === "active") {
@@ -62,6 +61,7 @@ export async function GET(request) {
         learners.first_name LIKE $${queryParameters.length + 1} OR
         learners.last_name LIKE $${queryParameters.length + 1}
     )`;
+    queryParameters.push(`${search}%`);
   }
 
   sqlQuery += `
