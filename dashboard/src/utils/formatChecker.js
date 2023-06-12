59ed03efcc53cdade47f94f4b3ecdef00fe19cf1
KobeVasquezDI <86027430+KobeVasquezDI@users.noreply.github.com>
2023-06-13 10:50:09 +1200

    added comment to formatChecker.js

--- dashboard/src/utils/formatChecker.js
+++ dashboard/src/utils/formatChecker.js
@@ -1,3 +1,4 @@
+// do not use for the [id] routes until solution is found.
 const formatChecker = (input) => {
   const regex = new RegExp(
     /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
